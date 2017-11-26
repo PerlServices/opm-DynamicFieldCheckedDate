003d1de99f17e100932a4d22e9c8bfe2c01982ce
reneeb <info@perl-services.de>
2017-11-26 11:02:45 +0100

    make CheckedDate* fields work with process tickets

--- Kernel/System/DynamicField/Driver/CheckedDate.pm
+++ Kernel/System/DynamicField/Driver/CheckedDate.pm
@@ -56,12 +56,21 @@ sub EditFieldValueGet {
     # pre-check the field when
     #   - only an action is given
     #   - no action at all
-    my $PreCheck = 0;
+    my $PreCheck  = 0;
+    my $Action    = '';
+    my $Subaction = '';
+
+    if ( $Param{ParamObject} ) {
+        $Action    = $Param{ParamObject}->GetParam( Param => 'Action' )    || '';
+        $Subaction = $Param{ParamObject}->GetParam( Param => 'Subaction' ) || '';
+    }
 
     if ( !$DynamicFieldValues{ $Prefix . 'Used' }
         && !$DynamicFieldValues{ $Prefix . 'Year' }
         && !$DynamicFieldValues{ $Prefix . 'Month' }
         && !$DynamicFieldValues{ $Prefix . 'Day' }
+        && $Action !~ m{TicketProcess\z}
+        && $Subaction ne 'DisplayActivityDialog'
         )
     {
         $PreCheck = 1;
